caea8d0393957482255afaed6e0de05c187879fa
Samuel Pfrommer <sam.pfrommer@gmail.com>
2021-06-12 13:24:14 -0400

    Successful test code connecting to mongodb server

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,25 @@
 'use strict';
 
 const express = require('express');
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const uri = "mongodb://mongodb:27017";
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Constants
 const PORT = 8081;
