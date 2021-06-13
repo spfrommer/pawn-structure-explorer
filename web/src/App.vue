c0330969f221e10f610bc64da2d17f280d27d5cc
Samuel Pfrommer <sam.pfrommer@gmail.com>
2021-06-12 23:54:34 -0400

    Add metal background

--- web/src/App.vue
+++ web/src/App.vue
@@ -16,8 +16,10 @@ export default {
     data: function() {
         return {
             highlights: {
-                colormap: interpolate(['rgba(2,0,36,0.5)', 'rgba(193,103,255,0.5)', 'rgba(0,212,255,0.5)']),
-                intensities: this.$utils.zeros(8, 8)
+                // colormap: interpolate(['rgba(2,0,36,0.0)', 'rgba(193,103,255,0.0)', 'rgba(0,212,255,0.0)']),
+                // colormap: interpolate(['rgba(193,103,255,0.2)', 'rgba(0,212,255,0.2)']),
+                colormap: interpolate(['rgba(0, 255, 254, 0.2)', 'rgba(222, 0, 255, 0.2)']),
+                intensities: this.$utils.random(8, 8)
             }
         }
     },
--- web/src/utils.js
+++ web/src/utils.js
@@ -2,4 +2,7 @@ export default {
     zeros: function(rows, columns) {
         return Array(rows).fill().map(() => Array(columns).fill(0));
     },
+    random: function(rows, columns) {
+        return Array(rows).fill().map(() => Array.from({length: columns}, () => Math.random()));
+    },
 };
