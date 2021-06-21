246eec12a91681bd1a3353018be7001d13c01e9f
Samuel Pfrommer <sam.pfrommer@gmail.com>
2021-06-21 12:27:55 -0400

    Deployment to server working!

--- coord.sh
+++ coord.sh
@@ -10,6 +10,7 @@ if [ "$MODE" == "INIT" ]; then
     echo ">>>>> INSTALLING DOCKER COMPOSE ON MANAGER <<<<<"
     gcloud compute ssh main --command="sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose"
     gcloud compute ssh main --command="sudo chmod +x /usr/local/bin/docker-compose"
+    gcloud compute ssh main --command="sudo apt-get install unzip"
 
     echo ">>>>> CREATING SWARM <<<<<"
     MGR=$(gcloud compute instances describe main --format='get(networkInterfaces[0].networkIP)')
@@ -24,14 +25,22 @@ if [ "$MODE" == "INIT" ]; then
     gcloud compute ssh main --command="sudo docker network create nwmain --driver overlay --attachable"
 fi
 
-echo ">>>>> SETTING UP MANAGER AND BUILDING IMAGES <<<<<"
-gcloud compute ssh main --command="sudo rm -rf ~/pawn-structure-explorer"
-gcloud compute ssh main --command="git clone https://github.com/spfrommer/pawn-structure-explorer.git"
+echo ">>>>> FETCHING SOURCE <<<<<"
+if [ "$MODE" == "INIT" ]; then
+    gcloud compute ssh main --command="git clone https://github.com/spfrommer/pawn-structure-explorer.git"
+
+    echo ">>>>> FETCHING DATA <<<<<"
+    gcloud compute ssh main --command="cd pawn-structure-explorer/data/lichess-elite-database && curl https://database.nikonoel.fr/lichess_elite_2020-06.zip --output lichess_elite_2020-06.zip"
+    gcloud compute ssh main --command="cd pawn-structure-explorer/data/lichess-elite-database && unzip lichess_elite_2020-06.zip && rm lichess_elite_2020-06.zip"
+fi
+if [ "$MODE" == "REDEPLOY" ]; then
+    gcloud compute ssh main --command="cd pawn-structure-explorer && get pull"
+fi
+echo ">>>>> BUILDING IMAGES <<<<<"
 gcloud compute ssh main --command="cd pawn-structure-explorer && sudo docker-compose build"
 gcloud compute ssh main --command="sudo usermod -a -G docker ${USER}"
-gcloud compute ssh main --command="mkdir ~/pawn-structure-explorer/data/lichess-elite-database && mkdir ~/pawn-structure-explorer/data/mongodb"
 
 echo ">>>>> DEPLOYING <<<<<"
-gcloud compute ssh main --command="cd webtrace && sudo docker stack deploy --compose-file swarm.yml pawnse"
+gcloud compute ssh main --command="cd pawn-structure-explorer && sudo docker stack deploy --compose-file swarm.yml pawnse"
 # gcloud compute ssh main --command="cd webtrace && sudo docker service scale webtrace_queueworker=0 webtrace_fetchworker=0 webtrace_indexer=0"
 # docker run --network nw1 -it gcr.io/verdant-future-312705/cli:latest 
